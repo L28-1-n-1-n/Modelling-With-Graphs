4781565680781a245b696dfc55c1734135c07513
L28-1-n-1-n <zto2.71828@gmail.com>
2020-02-23 23:24:43 +0000

    removing comments

--- greedy_col_variation_incomplete.py
+++ greedy_col_variation_incomplete.py
@@ -10,31 +10,16 @@ import graph5
 def find_next_vertex(G):
     n = len(G.nodes())
     for node in range(2, n + 1):
-        print(node)
         if (node not in visited_counter):
-            print(node)
             for connection in G[node]:
                 if (connection in visited_counter):
                     return (node)
 
 
-
-
 def find_smallest_colour(G,i):
     n = len(G.nodes())
     neighbour_colours = set(colour_map.get(connection) for connection in G[i])
-    print("roll")
-    for connection in G[i]:
-        print("i is " + str(i))
-        print(colour_map.get(connection))
-        print("connection is :")
-        print(connection)
-    print("hola")
-    print(i)
-    print(neighbour_colours)
     return (next(colour for colour in range(n) if colour not in neighbour_colours))
-
-
 
 
 def greedy(G):
@@ -52,24 +37,16 @@ def greedy(G):
     visited_counter.append(1)
     while (len(visited_counter) != len(G.nodes())):
         node = find_next_vertex(G)
-        print("next node is :")
-        print(node)
         colour_map[node] = find_smallest_colour(G, node)
         visited_counter.append(node)
-
 
     out_map = []
     for node in G.nodes():
         G.nodes[node]['colour'] = colour_map[node]
         out_map.append(colour_map[node])
     kmax = max(colour_map.values()) + 1
-    #     # for debugging: show colour of each node
 
-    print(colour_map)
-    print(nx.info(G))
-    nx.draw(G, node_color=out_map, with_labels=True)
-    plt.show()
-    # end of debugging
+    # for debugging: show colour of each node
 
     print()
     for i in G.nodes():
@@ -78,77 +55,12 @@ def greedy(G):
     print('The number of colours that Greedy computed is:', kmax)
     print()
 
-
-
-
-#
-#
-# def find_smallest_colour(G,i):
-#     n = len(G.nodes())
-#     neighbour_colours = set(colour_map.get(connection) for connection in G[i])
-#     print("roll")
-#     for connection in G[i]:
-#         print (colour_map.get(connection))
-#     print("hola")
-#     print(i)
-#     print(neighbour_colours)
-#     return (next(colour for colour in range(n) if colour not in neighbour_colours))
-#
-#
-#
-#
-# def greedy(G):
-#     global kmax
-#
-#     global colour_map
-#
-#     colour_map ={}
-#     for node in G.nodes():
-#         # neighbour_colours = set(colour_map.get(neighbour) for neighbour in G[node])
-#         # colour_map[node] = next(colour for colour in range(len(G)) if colour not in neighbour_colours)
-#         colour_map[node] = find_smallest_colour(G, node)
-#
-    # out_map = []
-    # for node in G.nodes():
-    #     G.nodes[node]['colour'] = colour_map[node]
-    #     out_map.append(colour_map[node])
-
-#     print()
-#     for i in G.nodes():
-#         print('vertex', i, ': colour', G.nodes[i]['colour'])
-#     print()
-#
-#     # kmax value is max + 1 since colour codes start with zero
-#     kmax = max(colour_map.values()) + 1
-#     print('The number of colours that Greedy computed is:', kmax)
-#
-#     # for debugging: show colour of each node
-
-    # print(colour_map)
-    # print (nx.info(G))
-    # nx.draw(G, node_color=out_map, with_labels=True)
-    # plt.show()
-    # # end of debugging
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    # for debugging: show colour of each node
+    print(colour_map)
+    print(nx.info(G))
+    nx.draw(G, node_color=out_map, with_labels=True)
+    plt.show()
+    # end of debugging
 
 print('Graph G1:')
 G=graph1.Graph()
